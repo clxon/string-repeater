5676f02ae8a18cf20bac91222cc9f7ccfe6f2b1e
Nicklaus Ning <Deeplemon@163.com>
2020-05-12 09:47:18 +0800

    Make the repeater

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿var repeatTheString = function(){
+    var input = $('#before').val()
+    var num = window.parseInt($('#num').val()) || 2
+    var result = ''
+    for(var i = 0;i<num;i++){
+        result += input
+    }
+    $('#result').text(result)
+}
